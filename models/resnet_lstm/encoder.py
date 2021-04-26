4631a0c3b88caf4c4a25abf66e5998732f7801fe
eunjeeSung <eunjee.lynn.sung@gmail.com>
2021-04-26 04:51:59 +0000

    Add CRAFT encoder experiment

--- models/resnet_lstm/encoder.py
+++ models/resnet_lstm/encoder.py
@@ -1,17 +1,120 @@
 import torch
 import torch.nn as nn
 import torchvision
+import torch.nn.functional as F
+
+from .layers import ConvPoolBlock, DoubleConv, Down, Up, OutConv, double_conv, vgg16_bn
+
+
+# class EncoderCNNtrain18(nn.Module):
+#     def __init__(self, in_channels, encoder_dim):
+#         super(EncoderCNNtrain18, self).__init__()
+#         resnet = torchvision.models.resnet18()
+#         modules = list(resnet.children())[:-2]
+#         self.resnet = nn.Sequential(*modules)        
+
+#     def forward(self, images):
+#         features = self.resnet(images) 
+#         features = features.permute(0, 2, 3, 1)                           #(batch_size,8,8,512)
+#         features = features.view(features.size(0), -1, features.size(-1)) #(batch_size,64,512)
+#         return features
+
+
+# class EncoderCNNtrain18(nn.Module):
+#     def __init__(self, in_channels, encoder_dim):
+#         super(EncoderCNNtrain18, self).__init__()
+#         self.conv1 = nn.Conv2d(in_channels, encoder_dim, 5, stride=(1, 1))
+#         self.conv2 = nn.Conv2d(encoder_dim, encoder_dim, 5, stride=(1, 1))
+#         self.conv_pool1 = ConvPoolBlock(encoder_dim, encoder_dim)
+#         self.conv_pool2 = ConvPoolBlock(encoder_dim, encoder_dim)
+#         self.conv_pool3 = ConvPoolBlock(encoder_dim, encoder_dim)
+#         self.conv_pool4 = ConvPoolBlock(encoder_dim, encoder_dim)
+
+#     def forward(self, images):
+#         features = self.conv1(images)
+#         features = self.conv2(features)
+#         features = self.conv_pool1(features)   
+#         features = self.conv_pool2(features)
+#         features = self.conv_pool3(features)
+#         features = self.conv_pool4(features)                     #(batch_size,512,8,8)
+
+#         features = features.permute(0, 2, 3, 1)                           #(batch_size,8,8,512)
+#         features = features.view(features.size(0), -1, features.size(-1)) #(batch_size,64,512)
+           
+#         return features        
+
+
+# class EncoderCNNtrain18(nn.Module):
+#     def __init__(self, in_channels, encoder_dim, bilinear=True):
+#         super(EncoderCNNtrain18, self).__init__()
+#         self.in_channels = in_channels
+#         self.encoder_dim = encoder_dim
+#         self.bilinear = bilinear
+
+#         self.inc = DoubleConv(in_channels, 64)
+#         self.down1 = Down(64, 128)
+#         self.down2 = Down(128, 256)
+#         self.down3 = Down(256, 512)
+#         factor = 2 if bilinear else 1
+#         self.down4 = Down(512, 1024 // factor)
+#         self.up1 = Up(1024, 512 // factor, bilinear)
+#         self.up2 = Up(512, 256 // factor, bilinear)
+#         self.up3 = Up(256, 128 // factor, bilinear)
+#         self.up4 = Up(128, 64, bilinear)
+#         self.outc = OutConv(64, encoder_dim)
+
+#     def forward(self, x):
+#         x1 = self.inc(x)
+#         x2 = self.down1(x1)
+#         x3 = self.down2(x2)
+#         x4 = self.down3(x3)
+#         x5 = self.down4(x4)
+#         x = self.up1(x5, x4)
+#         x = self.up2(x, x3)
+#         x = self.up3(x, x2)
+#         x = self.up4(x, x1)
+#         features = self.outc(x)
+
+#         features = features.permute(0, 2, 3, 1)                           #(batch_size,8,8,512)
+#         features = features.view(features.size(0), -1, features.size(-1)) #(batch_size,64,512)
+         
+#         return features
 
 
 class EncoderCNNtrain18(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels, encoder_dim, pretrained=True, freeze=True):
         super(EncoderCNNtrain18, self).__init__()
-        resnet = torchvision.models.resnet18()
-        modules = list(resnet.children())[:-2]
-        self.resnet = nn.Sequential(*modules)
 
-    def forward(self, images):
-        features = self.resnet(images)                                    #(batch_size,512,8,8)
+        """ Base network """
+        self.basenet = vgg16_bn(pretrained, freeze)
+
+        """ U network """
+        self.upconv1 = double_conv(1024, 512, 256)
+        self.upconv2 = double_conv(512, 256, 128)
+        self.upconv3 = double_conv(256, 128, 64)
+        self.upconv4 = double_conv(128, 64, encoder_dim)
+        
+    def forward(self, x):
+        """ Base network """
+        sources = self.basenet(x)
+
+        """ U network """
+        y = torch.cat([sources[0], sources[1]], dim=1)
+        y = self.upconv1(y)
+
+        y = F.interpolate(y, size=sources[2].size()[2:], mode='bilinear', align_corners=False)
+        y = torch.cat([y, sources[2]], dim=1)
+        y = self.upconv2(y)
+
+        y = F.interpolate(y, size=sources[3].size()[2:], mode='bilinear', align_corners=False)
+        y = torch.cat([y, sources[3]], dim=1)
+        y = self.upconv3(y)
+
+        y = F.interpolate(y, size=sources[4].size()[2:], mode='bilinear', align_corners=False)
+        y = torch.cat([y, sources[4]], dim=1)
+        features = self.upconv4(y)
+
         features = features.permute(0, 2, 3, 1)                           #(batch_size,8,8,512)
         features = features.view(features.size(0), -1, features.size(-1)) #(batch_size,64,512)
+
         return features
---
+++ models/resnet_lstm/layers.py
@@ -0,0 +1,179 @@
+
+from collections import namedtuple
+
+
+import torch
+import torch.nn as nn
+import torchvision
+import torch.nn.functional as F
+from torchvision import models
+from torchvision.models.vgg import model_urls
+
+
+class ConvPoolBlock(nn.Module):
+    def __init__(self, in_channels, encoder_dim):
+        super(ConvPoolBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, encoder_dim, 5, stride=(1, 1))
+        self.bn = nn.BatchNorm2d(encoder_dim)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        return x
+
+
+class Classifier(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Classifier, self).__init__()
+        self.fc1 = nn.Linear(in_channels, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, out_channels)
+        
+        self.in_channels = in_channels
+
+    def forward(self, x):
+        x = x.reshape(-1, 64 * 512)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels , in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        # if you have padding issues, see
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class double_conv(nn.Module):
+    def __init__(self, in_ch, mid_ch, out_ch):
+        super(double_conv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch + mid_ch, mid_ch, kernel_size=1),
+            nn.BatchNorm2d(mid_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class vgg16_bn(nn.Module):
+    def __init__(self, pretrained=True, freeze=True):
+        super(vgg16_bn, self).__init__()
+        model_urls['vgg16_bn'] = model_urls['vgg16_bn'].replace('https://', 'http://')
+        vgg_pretrained_features = models.vgg16_bn(pretrained=pretrained).features
+        self.slice1 = torch.nn.Sequential()
+        self.slice2 = torch.nn.Sequential()
+        self.slice3 = torch.nn.Sequential()
+        self.slice4 = torch.nn.Sequential()
+        self.slice5 = torch.nn.Sequential()
+        for x in range(12):         # conv2_2
+            self.slice1.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(12, 19):         # conv3_3
+            self.slice2.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(19, 29):         # conv4_3
+            self.slice3.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(29, 39):         # conv5_3
+            self.slice4.add_module(str(x), vgg_pretrained_features[x])
+
+        # fc6, fc7 without atrous conv
+        self.slice5 = torch.nn.Sequential(
+                nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+                nn.Conv2d(512, 1024, kernel_size=3, padding=6, dilation=6),
+                nn.Conv2d(1024, 1024, kernel_size=1)
+        )
+
+        if freeze:
+            for param in self.slice1.parameters():      # only first conv
+                param.requires_grad= False
+
+    def forward(self, X):
+        h = self.slice1(X)
+        h_relu2_2 = h
+        h = self.slice2(h)
+        h_relu3_2 = h
+        h = self.slice3(h)
+        h_relu4_3 = h
+        h = self.slice4(h)
+        h_relu5_3 = h
+        h = self.slice5(h)
+        h_fc7 = h
+        vgg_outputs = namedtuple("VggOutputs", ['fc7', 'relu5_3', 'relu4_3', 'relu3_2', 'relu2_2'])
+        out = vgg_outputs(h_fc7, h_relu5_3, h_relu4_3, h_relu3_2, h_relu2_2)
+        return out
