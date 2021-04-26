2833011b3b72f0a9fc80d4b04a3ff3cafafa976b
eunjeeSung <eunjee.lynn.sung@gmail.com>
2021-04-26 04:53:32 +0000

    Try experiment for separate fields

--- data.py
+++ data.py
@@ -10,7 +10,7 @@ from torchvision import get_image_backend
 from util import get_train_file_path, get_test_file_path, string_to_ints
 
 class InputDatasetTest(Dataset):
-    def __init__(self, df, transform, stoi, is_train=True):
+    def __init__(self, df, transform, stoi, is_train=True, *args, **kwargs):
         self.img_paths = get_train_file_path(df['image_id']) if is_train \
                             else get_test_file_path(df['image_id'])
         self.inchis = df['InChI']
@@ -25,11 +25,23 @@ class InputDatasetTest(Dataset):
         sample = self.loader(self.img_paths[idx])
         sample = self.transform(sample)
 
-        #numericalize the caption text
-        inchi_str = self.inchis[idx]
+        # (1) For whole InChI text
+        #inchi_str = self.inchis[idx][9:]
+        # inchi_vec = string_to_ints(inchi_str, self.stoi)
+        
+        # (2) For chemical note generator
+        inchi_str = self.inchis[idx].split('/')[1]
         inchi_vec = string_to_ints(inchi_str, self.stoi)
 
-        return sample, torch.tensor(inchi_vec), idx
+        # (3) For m
+        # for layerid, layer in enumerate(['m']):
+        #     inchi_str = [ ''.join([splitlayer if splitlayer[0]==layer else "" for splitlayer in self.inchis[idx].split("/")])
+        #                         for train_label in self.inchis[idx]]
+        #     inchi_str = ['' if len(item)==0 else item[1:] for item in inchi_str][0]
+        # mtoi = {'': 0, '0': 1, '0m1': 2, '1': 3, '1m0': 4}
+        # inchi_vec = [mtoi[inchi_str]]
+
+        return sample, torch.tensor(inchi_vec), torch.tensor(idx)
 
 
 def pil_loader(path: str) -> Image.Image: #copied from torchvision
--- models/resnet_lstm/resnet_lstm.py
+++ models/resnet_lstm/resnet_lstm.py
@@ -4,12 +4,12 @@ import torchvision
 
 from .encoder import EncoderCNNtrain18
 from .decoder import DecoderRNN
-
+from .layers import Classifier
 
 class EncoderDecodertrain18(nn.Module):
     def __init__(self,embed_size, vocab_size, attention_dim,encoder_dim,decoder_dim,drop_prob=0.3):
         super().__init__()
-        self.encoder = EncoderCNNtrain18()
+        self.encoder = EncoderCNNtrain18(3, encoder_dim)
         self.decoder = DecoderRNN(
             embed_size=embed_size,
             vocab_size = vocab_size,
@@ -17,8 +17,17 @@ class EncoderDecodertrain18(nn.Module):
             encoder_dim=encoder_dim,
             decoder_dim=decoder_dim
         )
+        self.m_classifier = Classifier(64 * 512, 5)
         
     def forward(self, images, captions):
         features = self.encoder(images)
         outputs = self.decoder(features, captions)
+
+        # for m generation
+        # outputs = self.m_classifier(features)
         return outputs
+
+    def evaluate(self, images, stoi, itos):
+        features = self.encoder(images)
+        captions = self.decoder.generate_caption(features, stoi=stoi, itos=itos)
+        return captions
