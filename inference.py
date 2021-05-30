531a9b90cd2fe2d133bdda35d16f6fa3e1389933
Ubuntu <ubuntu@ip-172-31-55-87.ec2.internal>
2021-05-30 09:31:37 +0000

    Add inference code

--- inference.py
+++ inference.py
@@ -1,44 +1,59 @@
+import csv
+import os
+from typing import Any, Callable, cast, Dict, List, Optional, Tuple
+import yaml
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import time
+import PIL
+from PIL import Image
+from matplotlib import pyplot as plt
+from tqdm.auto import tqdm
+import nltk
+
 import torch
 import torch.nn as nn
 import torchvision
-from torch.utils.data import DataLoader
-
-import pandas as pd
-import yaml
-from tqdm import tqdm
+from torch.utils.data import DataLoader, random_split
+import torch.optim as optim
 
 from data import InputDatasetTest
+from loss import LabelSmoothCrossEntropyLoss
+from util import CapsCollateExperiment, get_train_file_path, get_test_file_path, \
+                 tensor_to_captions, save_model, transform, get_score, tensor_to_captions_multihead
 from models.resnet_lstm import EncoderDecodertrain18
-from util import CapsCollate, tensor_to_captions, transform
+from models.satrn import SATRNTrnasformer
+from vocab import general_to_i, t_to_i, m_to_i, s_to_i,\
+                     i_to_general, i_to_t, i_to_m, i_to_s
+
 
 
 if __name__ == "__main__":
-    with open('configs.yaml') as file:
-        cfgs = yaml.load(file, Loader=yaml.FullLoader)
+    torch.manual_seed(42)
 
+    # Get train and test set
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    test = pd.read_csv('./input/sample_submission.csv')            
-    train = pd.read_csv('./input/train_labels.csv')
 
-    # Make vocab
-    # TODO: store vocabs
-    words=set()
-    for st in train['InChI']:
-        words.update(set(st))
-    print(f'words length: {len(words)}')
-    del(train)
+    # Load training dataset
+    data = pd.read_csv('./input/sample_submission.csv', dtype=str).fillna('')
+    print(f'test.shape: {data.shape}')
 
-    vocab=list(words)
-    vocab.append('<sos>')
-    vocab.append('<eos>')
-    vocab.append('<pad>')
-    stoi={'C': 0,')': 1,'P': 2,'l': 3,'=': 4,'3': 5,'N': 6,'I': 7,'2': 8,'6': 9,'H': 10,'4': 11,'F': 12,'0': 13,'1': 14,'-': 15,'O': 16,'8': 17,
-    ',': 18,'B': 19,'(': 20,'7': 21,'r': 22,'/': 23,'m': 24,'c': 25,'s': 26,'h': 27,'i': 28,'t': 29,'T': 30,'n': 31,'5': 32,'+': 33,'b': 34,'9': 35,
-    'D': 36,'S': 37,'<sos>': 38,'<eos>': 39,'<pad>': 40}
-    itos={item[1]:item[0] for item in stoi.items()}
 
-    vocab_size = len(vocab) + 10 # TODO: remove
+    # Vocabulary
+    dictionaries = (general_to_i, general_to_i, m_to_i, s_to_i)
+    inverse_dictionaries = (i_to_general, i_to_general, i_to_m, i_to_s)
+
+
+    # Hyperparameters
+    with open('configs.yaml') as file:
+        # The FullLoader parameter handles the conversion from YAML
+        # scalar values to Python the dictionary format
+        cfgs = yaml.load(file, Loader=yaml.FullLoader)
+
+    vocab_size = len(general_to_i) # TODO: remove
     embed_size = cfgs['embed_size']
     attention_dim = cfgs['attention_dim']
     encoder_dim = cfgs['encoder_dim']
@@ -48,56 +63,82 @@ if __name__ == "__main__":
     batch_size = cfgs['batch_size']
     num_workers = cfgs['num_workers']
     learning_rate = cfgs['learning_rate']
-    pad_idx = stoi["<pad>"]    
-    # TODO: implement official loss function
-    criterion = nn.CrossEntropyLoss(ignore_index=stoi["<pad>"])
+    pad_idx = general_to_i["<pad>"]
     is_sampling_mode = cfgs['is_sampling_mode']
-    sample_size = cfgs['sample_size']
+    sample_size = cfgs['sample_size']    
 
-    if is_sampling_mode:
-        test = test.sample(n=sample_size, random_state=1)
-        test = test.reset_index()
-
-
-    # Model
-    MODEL_PATH = './checkpoints/sample.pth'    
-    model = EncoderDecodertrain18(
-        embed_size=embed_size,
-        vocab_size = vocab_size,
-        attention_dim=attention_dim,
-        encoder_dim=encoder_dim,
-        decoder_dim=decoder_dim    
-    )
-    model.to(device)
-    # TODO: load hyperparameters
-    model.load_state_dict(torch.load(MODEL_PATH)['state_dict'])
-    
-    dataset_test = InputDatasetTest(test, transform, stoi, is_train=False)
-    dataloader_test = DataLoader(
-        dataset=dataset_test,
+    dataset = InputDatasetTest(data, transform, dictionaries, is_train=False)
+    dataloader = DataLoader(
+        dataset=dataset,
         batch_size=batch_size,
         shuffle=False,
         num_workers=num_workers,
-        collate_fn=CapsCollate(pad_idx=pad_idx, batch_first=True))
+        drop_last=False)
 
-    model.eval()
-    losses = []
-    with torch.no_grad():
-        for i, (images, inchis, indices) in enumerate(tqdm(dataloader_test)):
-            images, inchis = images.to(device), inchis.to(device)
-            features = model.encoder(images)
-            caps = model.decoder.generate_caption(features, stoi=stoi, itos=itos)
-            captions = tensor_to_captions(caps, stoi=stoi, itos=itos)
-            test['InChI'].loc[indices] = captions
-            if i % print_every == 0:
-                # TODO: only pass caption length to the model
-                outputs = model(images, inchis)
-                targets = inchis[:, 1:].to(device)
-                loss = criterion(outputs.view(-1, vocab_size), targets.reshape(-1))
-                losses.append(loss)
-                print(f'Example output: {captions[0]} / Example loss: {loss}')
 
-    print(f'Average loss: {sum(losses) / len(losses)}, #loss: {len(losses)}')
-    output_cols = ['image_id', 'InChI']
-    test[output_cols].to_csv('submission.csv',index=False)
-    print(test[output_cols].head())
+    #Main model
+    model = EncoderDecodertrain18(
+        embed_size=embed_size,
+        vocab_size=vocab_size,
+        attention_dim=attention_dim,
+        encoder_dim=encoder_dim,
+        decoder_dim=decoder_dim
+    )
+    model.to(device)
+
+
+    # Load model
+    model.load_state_dict(
+        torch.load('./checkpoints/attention_model_state_setting30_623600.pth')
+        ['state_dict']
+    )
+
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)  
+    criterion = nn.CrossEntropyLoss(ignore_index=general_to_i["<pad>"])
+   
+    inchis = []   
+    for idx, images in enumerate(tqdm(dataloader)):
+        images = images.to(device)
+
+        # For ResNet+LSTM
+        caption_tensors = model.evaluate(images, dictionaries, inverse_dictionaries)
+        captions = tensor_to_captions_multihead(caption_tensors, dictionaries, inverse_dictionaries)
+
+        # Collect InChI strings
+        for note, c, h, b, t, m, s, i in zip(*list(captions)):
+            inchi_str = "InChI=1S"
+            # (1) Chemical note: concat as-is
+            inchi_str += f"/{note}"
+
+            # (2) C layer: insert hyphens between numbers
+            inchi_str += f"/c{c}"
+
+            # (3) H layer: concat as-is
+            inchi_str += f"/h{h}"
+
+            # (4) B layer: concat as-is if exists
+            if b != '':
+                inchi_str += f"/b{b}"
+
+            # (5) T layer: concat as-is if exists
+            if t != '':
+                inchi_str += f"/t{t}"
+
+            # (6) M layer: {'', '0', '0m1', '1', '1m0'} concat in integer form if exsits
+            if m != '':
+                inchi_str += f"/m{m}"
+
+            # (7) S layer: {'', '1'} concat in integer form if exsits
+            if s != '':
+                inchi_str += f"/s{int(float(s))}"
+
+            # (8) I layer: concat as-is if exists
+            if i != '':
+                inchi_str += f"/i{i}"
+
+            # Collect
+            inchis.append(inchi_str)
+
+    output_df = pd.read_csv('./input/sample_submission.csv', dtype=str)
+    output_df['InChI'] = inchis
+    output_df[['image_id', 'InChI']].to_csv('./input/sample_submission_result.csv', index=False)
